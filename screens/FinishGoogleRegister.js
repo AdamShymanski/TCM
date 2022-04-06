b1a30ad27142f49389f01ed4e86d16227afbbb6d
Adam Szymański <szymanskiadam111@gmail.com>
2022-04-06 22:20:53 +0200

    add notificationToken

--- screens/FinishGoogleRegister.js
+++ screens/FinishGoogleRegister.js
@@ -101,6 +101,7 @@ export default function FinishGoogleRegister({ callback, name }) {
                 vendorId: null,
                 merchantId: null,
               },
+              notificationToken: null,
               savedOffers: [],
               createdAt: firebase.firestore.FieldValue.serverTimestamp(),
             });
